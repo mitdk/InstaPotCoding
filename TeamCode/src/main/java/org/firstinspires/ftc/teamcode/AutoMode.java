9a2857416a42da3ebcf6b429ec74754b8292874f
Unnat <iprobotics@gmail.com>
2024-11-02 10:20:13 -0700

    added MecanumDrive class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -1,4 +1,23 @@
 package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
 import com.acmerobotics.roadrunner.*;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,14 +30,19 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 @Autonomous(name = "AutoMode")
 
 public class AutoMode extends LinearOpMode {
+    public class Drivetrain{
+        public void MecanumDrive(HardwareMap hardwareMap) {
 
-    MecanumDrive drive = new MecanumDrive(hardwareMap);
+        }
+    }
+
 
 
 
