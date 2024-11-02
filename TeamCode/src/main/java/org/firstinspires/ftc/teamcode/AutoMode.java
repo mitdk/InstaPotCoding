936d5a7279f0feab02c259f992eac92b060e7dff
Unnat <iprobotics@gmail.com>
2024-11-02 10:43:55 -0700

    initialized linslides, arm, servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -1,15 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
-import androidx.annotation.NonNull;
-
 // RR-specific imports
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
 
 // Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -17,34 +8,52 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import com.acmerobotics.roadrunner.*;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
+        import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
+        import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
 @Autonomous(name = "AutoMode")
 
 public class AutoMode extends LinearOpMode {
-    public class Drivetrain{
-        public void MecanumDrive(HardwareMap hardwareMap) {
+    public class Arm {
+        private DcMotorEx Arm;
 
+        public Arm(HardwareMap hardwareMap) {
+            Arm = hardwareMap.get(DcMotorEx.class, "armMotor");
+            Arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            Arm.setDirection(DcMotorSimple.Direction.FORWARD);
         }
     }
+    public class Intake{
+        private CRServo Intake;
 
+        public Intake(HardwareMap hardwareMap) {
+            Intake = hardwareMap.get(CRServo.class, "Intake");
+        }
+    }
+    public class Wrist{
+        private Servo Wrist;
 
+        public Wrist(HardwareMap hardwareMap) {
+            Wrist = hardwareMap.get(Servo.class, "Servo");
+        }
+    }
+    public class LinearSlides{
+        private DcMotorEx LinSlideLeft;
+        private DcMotorEx LinSlideRight;
 
+        public void LinSlideLeft(HardwareMap hardwareMap) {
+            LinSlideLeft = hardwareMap.get(DcMotorEx.class, "LinearSlideLeft");
+            LinSlideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            LinSlideLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+        public void LinSlideRight(HardwareMap hardwareMap) {
+            LinSlideRight = hardwareMap.get(DcMotorEx.class, "LinearSlideRight");
+            LinSlideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            LinSlideRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
