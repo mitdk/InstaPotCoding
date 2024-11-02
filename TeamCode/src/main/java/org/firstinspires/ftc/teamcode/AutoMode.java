ab73b5fae3495c77cabe605bce712ece02de5057
Unnat <iprobotics@gmail.com>
2024-11-02 11:33:30 -0700

    initialized linslides, arm, servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -1,17 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+
 // RR-specific imports
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
 
 // Non-RR imports
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
-        import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.CRServo;
+
 
 @Autonomous(name = "AutoMode")
 
@@ -39,22 +45,69 @@ public class AutoMode extends LinearOpMode {
             Wrist = hardwareMap.get(Servo.class, "Servo");
         }
     }
-    public class LinearSlides{
-        private DcMotorEx LinSlideLeft;
-        private DcMotorEx LinSlideRight;
+    public class LinearSlideLeft{
+        public DcMotorEx LinSlideLeft;
 
-        public void LinSlideLeft(HardwareMap hardwareMap) {
+        public LinearSlideLeft(HardwareMap hardwareMap) {
             LinSlideLeft = hardwareMap.get(DcMotorEx.class, "LinearSlideLeft");
             LinSlideLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             LinSlideLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         }
-        public void LinSlideRight(HardwareMap hardwareMap) {
-            LinSlideRight = hardwareMap.get(DcMotorEx.class, "LinearSlideRight");
+        public class LinSlideLeftUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+
+                }
+                double pos = LinSlideLeft.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos < 1) {
+
+                } else {
+                    LinSlideLeft.setPower(0);
+                    return false;
+                }
+                return false;
+            }
+        }
+        public Action LinSlideLeftUp() {
+            return new LinSlideLeftUp();
+        }
+    }
+    public class LinearSlideRight {
+        public DcMotorEx LinSlideRight;
+
+        public LinearSlideRight(HardwareMap hardwareMap) {
+            LinSlideRight = hardwareMap.get(DcMotorEx.class, "LinearSlideLeft");
             LinSlideRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             LinSlideRight.setDirection(DcMotorSimple.Direction.FORWARD);
         }
-    }
 
+        public class LinSlideRightUp implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+
+                }
+                double pos = LinSlideRight.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos < 1) {
+
+                } else {
+                    LinSlideRight.setPower(0);
+                    return false;
+                }
+                return false;
+            }
+        }
+        public Action LinSlideRightUp() {
+            return new LinSlideRightUp();
+        }
+    }
     @Override
     public void runOpMode() throws InterruptedException {
 
