d8974b727dd3d522c7484b35ffa51ef4b597124b
Unnat <iprobotics@gmail.com>
2024-11-03 08:31:09 -0800

    initialized linslides, arm, servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoMode.java
@@ -188,48 +188,30 @@ public class AutoMode extends LinearOpMode {
 
 
         TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
-                .strafeToLinearHeading(new Vector2d(3.7, -31.2), Math.toRadians(180))
+                //Moves towards submersible for initial specimen drop
+                .strafeToLinearHeading(new Vector2d(3.7, -31.3), Math.toRadians(180))
+                //Turns wrist servo 45 degrees, between the normal and specimen positions
                 .waitSeconds(0.7)
+                //Mskes specimen touch the bar, brings bot closer
                 .strafeToConstantHeading(new Vector2d(3.7, -28))
+                //Turns wrist 45 to complete full 90, specimen should be clipped on
+                //Intake outtakes to let go of specimen
                 .waitSeconds(2)
                 .strafeToConstantHeading(new Vector2d(3.7, -33.3))
+                //Brings robot to first sample block, the middle one of the 3 outside the sub
                 .splineToLinearHeading(new Pose2d(36.7, -25.8, Math.toRadians(0)), Math.toRadians(90))
+                //Intakes a sample block
                 .waitSeconds(2)
+                //Travel to basket
                 .strafeToConstantHeading(new Vector2d(36.7, -40))
                 .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)), Math.toRadians(180))
-                        .build());
-
-
-                /*.strafeToLinearHeading(new Vector2d(-55, -55), Math.toRadians(45))
+                /*.waitSeconds(2)
+                .lineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)))
                 .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(72, -40), Math.toRadians(90))
+                .lineToLinearHeading(new Pose2d(72, -40, Math.toRadians(90)))
                 .waitSeconds(2)
-                .strafeToLinearHeading(new Vector2d(66, -66), Math.toRadians(90));
-                                //Moves towards submersible for initial specimen drop
-                                .lineToLinearHeading(new Pose2d(3.7, -31.3, Math.toRadians(180)))
-                                //Turns wrist servo 45 degrees, between the normal and specimen positions
-                                .waitSeconds(0.7)
-                                //Mskes specimen touch the bar, brings bot closer
-                                .lineToConstantHeading(new Vector2d(3.7, -28))
-                                //Turns wrist 45 to complete full 90, specimen should be clipped on
-                                //Intake outtakes to let go of specimen
-                                .waitSeconds(2)
-                                .lineToConstantHeading(new Vector2d(3.7, -33.3))
-                                //Brings robot to first sample block, the middle one of the 3 outside the sub
-                                .splineToLinearHeading(new Pose2d(36.7, -25.8, Math.toRadians(0)), Math.toRadians(90))
-                                //Intakes a sample block
-                                .waitSeconds(2)
-                                //Travel to basket
-                                .lineToConstantHeading(new Vector2d(36.7, -40))
-                                .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)), Math.toRadians(180))
-                        /*.waitSeconds(2)
-                        .lineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)))
-                        .waitSeconds(2)
-                        .lineToLinearHeading(new Pose2d(72, -40, Math.toRadians(90)))
-                        .waitSeconds(2)
-                        .lineToLinearHeading(new Pose2d(66,-66, Math.toRadians(90)))*/
-
-                        waitForStart();
+                .lineToLinearHeading(new Pose2d(66,-66, Math.toRadians(90)))*/;
+                waitForStart();
         if(isStopRequested()) return;
         while(opModeIsActive()){
             Actions.runBlocking(
