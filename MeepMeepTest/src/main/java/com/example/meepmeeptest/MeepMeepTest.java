385a8b5a3e2b21840c9fb96a768ad6f0aafd72ae
Your Name <you@example.com>
2024-11-03 08:48:55 -0800

    Merge remote-tracking branch 'origin/master'

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTest.java
@@ -20,15 +20,15 @@ public class MeepMeepTest {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(90, 60, Math.toRadians(180), Math.toRadians(180), 15)
-
-                .followTrajectorySequence(drive ->
+//Right Red
+                /* .followTrajectorySequence(drive ->
                         drive.trajectorySequenceBuilder(new Pose2d(10, -66, 0))
 
                         //Moves towards submersible for initial specimen drop
                         .lineToLinearHeading(new Pose2d(3.7, -31.3, Math.toRadians(180)))
                         //Turns wrist servo 45 degrees, between the normal and specimen positions
                         .waitSeconds(0.7)
-                        //Mskes specimen touch the bar, brings bot closer
+                        //Makes specimen touch the bar, brings bot closer
                         .lineToConstantHeading(new Vector2d(3.7, -28))
                         //Turns wrist 45 to complete full 90, specimen should be clipped on
                         //Intake outtakes to let go of specimen
@@ -60,10 +60,40 @@ public class MeepMeepTest {
                         .lineToConstantHeading(new Vector2d(36.7, -40))
                         .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)), Math.toRadians(180))
                                 //Deposits 3rd sample block
+                        .waitSeconds(2) */
+//Left Red
+                .followTrajectorySequence(drive ->
+                drive.trajectorySequenceBuilder(new Pose2d(-10, -66, 0))
+                        .splineToLinearHeading(new Pose2d(-36.7, -25.8, Math.toRadians(180)), Math.toRadians(90))
+                         //Intakes a sample block
                         .waitSeconds(2)
+                        //Travel to basket
+
+                        .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)), Math.toRadians(180))
+                        //Deposits sample block
+                        .waitSeconds(2)
+                        //goes to 2nd sample
+                       .splineToLinearHeading(new Pose2d(-46.7, -25.8, Math.toRadians(180)), Math.toRadians(90))
+                       //Intakes 2nd sample block
+                       .waitSeconds(2)
+                       //Travel to basket
+
+                       .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)), Math.toRadians(180))
+                       //Deposits 3rd sample block
+                       .waitSeconds(2)
+                       //goes to 3rd sample
+                       .splineToLinearHeading(new Pose2d(-56.7, -25.8, Math.toRadians(180)), Math.toRadians(90))
+                       //Intakes 3rd sample block
+                       .waitSeconds(2)
+                       //Travel to basket
+
+                       .splineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)), Math.toRadians(180))
+                       //Deposits 3rd sample block
+                       .waitSeconds(2)
 
 
-                      /*  .lineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)))
+
+                                                /*  .lineToLinearHeading(new Pose2d(-55, -55, Math.toRadians(45)))
                         .waitSeconds(2)
                         .lineToLinearHeading(new Pose2d(72, -40, Math.toRadians(90)))
                         .waitSeconds(2)
